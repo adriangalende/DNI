3bc08245fc1d1705aff65cf8a24aab138b1969e6
Adrián <adriangalende@gmail.com>
2018-02-06 11:15:57 +0100

    Array dni correctos

--- test/java/org/mvpigs/dni/DniTest.java
+++ test/java/org/mvpigs/dni/DniTest.java
@@ -39,4 +39,16 @@ public class DNITest {
         DNI dni = new DNI("43182655V");
         assertEquals(true, dni.comprobarDNI());
     }
+
+    @Test
+    public void testArrayDNICorrectos() {
+        String[] dniCorrectos = { "43182655V", "43182653S", "28441270E", "50485512Y", "28441270E", "33291020S",
+                "63354599H", "33287887X", "37490778L", "04656946K", "89751615A", "51591203H" };
+
+        for (String documento : dniCorrectos) {
+            DNI dni = new DNI(documento);
+            assertEquals(true, dni.comprobarDNI());
+        }
+
+    }
 }
